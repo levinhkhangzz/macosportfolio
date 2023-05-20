58eb37e1b0af1489911599982b25aeb2a46f058d
Lê Vĩnh Khang <127677696+levinhkhangzz@users.noreply.github.com>
2023-05-20 20:47:17 +0700

    Update launchpad.ts

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,18 +2,6 @@ import type { LaunchpadData } from "~/types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
-  },
-  {
-    id: "oh-my-cv",
-    title: "Oh, My CV!",
-    img: "https://ohmycv.app/pwa-512x512.png",
-    link: "https://ohmycv.app"
-  },
-  {
     id: "oh-vue-icons",
     title: "Oh, Vue Icons!",
     img: "img/icons/launchpad/oh-vue-icons.png",
@@ -25,41 +13,17 @@ const launchpadApps: LaunchpadData[] = [
     img: "img/icons/launchpad/gungnir.png",
     link: "https://vuepress-theme-gungnir.vercel.app/"
   },
-  // {
-  //   id: "metallic",
-  //   title: "Metallic",
-  //   img: "img/icons/launchpad/meta.png",
-  //   link: "https://github.com/Renovamen/metallic"
-  // },
-  {
-    id: "what-if",
-    title: "My Notebook",
-    img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
-  },
   {
     id: "cube",
     title: "Cube Solver",
     img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
-  },
-  {
-    id: "resume",
-    title: "Résumé",
-    img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
+    link: "https://cube.levinhkhang.xyz"
   },
   {
     id: "zelda",
     title: "Webpage for Zelda",
     img: "img/icons/launchpad/zelda.png",
-    link: "https://zelda.zxh.io/"
-  },
-  {
-    id: "fishmail",
-    title: "Fishmail",
-    img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
+    link: "zedla.levinhkhang.xyz"
   }
 ];
 
